5d8a61426f486bce796b922cb6f6349a6402a940
rholz <rholz@uni.edu>
2018-09-12 16:53:10 -0500

    print_tokens.py now can print (some) tokens

--- src/print_tokens.py
+++ src/print_tokens.py
@@ -1,22 +1,36 @@
 from scanner import Scanner
 from flair_tokens import Token, TokenType
 '''
+print_tokens.py
+Uses the Flair scanner to make a human-readable
+list of tokens in a Flair program
+
+'''
+
+'''
 #file in
 fileName = argv[1]
 
 file = open(fileName)
 '''
 
-program = "program(a : int) end"
+program = "():=+-/*"
 
 #file to line
 
 #program = file.read()
-tokenList = Scanner(program)
-currentToken = tokenList.getNextToken()
-while currentToken.TokenType() not EOF:
-    
 
+#load the program into the scanner object
+tokenList = Scanner(program)
+#get the first token
+currentToken = tokenList.get_next_token()
+#print tokens and get the next, break if token is EOF
+while True:
+    print(currentToken)
+    if currentToken.is_eof():
+        break
+    currentToken = tokenList.get_next_token()
+    
 print("done scanning")
-print(tokenList)
+
 
