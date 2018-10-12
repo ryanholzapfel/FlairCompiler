8219f71204ff3093d059ec533b5ab001244aff11
Nicholas Rausch <rauscnaa@uni.edu>
2018-10-12 17:46:39 -0500

    Update sign.py

---
+++ programs/IsNeg.py
@@ -0,0 +1,7 @@
+program IsNeg( n : integer ) : boolean
+  begin
+    return if n < 0 then
+              true
+           else
+              false
+  end.
--- programs/sign.py
+++
@@ -1,7 +0,0 @@
-program SIGN( n : integer ) : boolean
-  begin
-    return if Positive(0, n) then
-              n > 0
-           else
-              Negative(n)
-  end.
