cc4b1ae93f1ae22451290339411a8f6b3917119c
Ryan Holzapfel <rholz@uni.edu>
2018-11-08 22:51:01 -0600

    upload codegen2.py

---
+++ src/codegen2.py
@@ -0,0 +1,81 @@
+from semanticactions import *
+
+class CodeGen(object):
+    def __init__(self, programNode, symbolTable):
+        self._programNode = programNode
+        self._symbolTable = symbolTable
+        self._programName = programNode.identifier().identifier()
+        self._jumpString = ""
+        self._currentLine = 0
+        self._programString = ""
+
+    def currentLine(self):
+        return self._currentLine
+
+    def incrementLine(self):
+        self._currentLine += 1
+
+    def addCode(self, code):
+        ln = self.currentLine()
+        self._programString = self._programString + currentLine + ": " + code + "\n"
+        self.incrementLine()
+
+    def genPointers(self):
+        self.addCode("LDC 5,-1(0)  #initialize status ptr")
+        self.addCode("LDC 6,2(0)   #initialize top ptr")
+
+    def savePointers(self):
+        pass
+    
+    def genProgramArgs(self):
+        self.addCode("")
+
+    def genMain(self):
+        pass
+
+    def saveReg(self):
+        self.addCode("ST 0,1(5)   #save IMEM to DMEM")
+        self.addCode("ST 1,2(5)   #save IMEM to DMEM")
+        self.addCode("ST 2,3(5)   #save IMEM to DMEM")
+        self.addCode("ST 3,4(5)   #save IMEM to DMEM")
+        self.addCode("ST 4,5(5)   #save IMEM to DMEM")
+
+    def loadReg(self):
+        self.addCode("LD 0,1(5)   #load DMEM to IMEM")
+        self.addCode("LD 1,2(5)   #load DMEM to IMEM")
+        self.addCode("LD 2,3(5)   #load DMEM to IMEM")
+        self.addCode("LD 3,4(5)   #load DMEM to IMEM")
+        self.addCode("LD 4,5(5)   #load DMEM to IMEM")
+
+    def initializeMain(self): #
+        self.genPointers()
+        self.storeReturn()
+        #hardcoded save and set status pointer
+        self.addCode("ST 5,7(6)")
+        self.addCode("LDA 5,1(6)")
+        self.addCode("ST 6,8(6)")
+        self.addCode("LDA 6,9(6)")
+        #end hardcode
+        self.genJump() #make a jump for later
+        self.addCode("LD#retrieve stored value")
+        
+    def storeReturn(self):
+        self.addCode("LDA 1,6(7)  #load return address")
+        self.addCode("ST 1,1(6)   #store return address")
+
+    def genJump(self): #WIP
+        self._jumpString = self._jumpString + self.currentLine() + ": LDA 7, X(0)"
+        self.incrementLine()
+
+    def returnMain(self):
+        self.addCode("OUT 2,0,0   #return result of main")
+        self.addCode("HALT 0,0,0  #stop execution; end of program")
+
+    def genMult(self, a,b,c): #r2 is possibly not zero
+        self.saveReg()
+        self.addCode("LDA 3,{}(0) #".format(a))
+        self.addCode("LD 4,{}(0)  #".format(b))
+        self.addCode("LD 5,{}(0)  #".format(c))
+        self.addCode("MUL 4,4,5   #multiply")
+        self.addCode("ST 4,0(3)   #store product in DMEM")
+        self.loadReg()
