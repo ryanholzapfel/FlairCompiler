2babaa8514002b32c27ecc3611aef764a5b97d27
rholz <rholz@uni.edu>
2018-11-26 13:48:23 -0600

    changed booleans to 0/1, added gen enum to 3ac

--- src/threeACGen.py
+++ src/threeACGen.py
@@ -1,5 +1,21 @@
 from semanticactions import *
 
+class GenExpression(Enum):
+    genMult = 1
+    genDiv = 2
+    genAdd = 3
+    genSubt = 4
+    genLess = 5
+    genEqual = 6
+    genOr = 7
+    genIf = 8
+    genAnd = 9
+    genNot = 10
+    genNeg = 11
+    genPrint = 12
+    genReturn = 13
+    genBool = 14
+
 class threeACGen(object):
     def __init__(self, programNode, symbolTable):
         self._programNode = programNode
@@ -11,3 +27,4 @@ class threeACGen(object):
         self._exprPrime.code =  [simpleExpr1.code]
                                 [simpleExpr2.code]
                                 emitCode(exprPrime.place,":=",simpleExpr1.place "<", simpleExpr2.place)
+
